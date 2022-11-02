4691229b8c5febaba559ac7ff0c9c6fbe260cd54
Rob Ranf <rob@emiyaconsulting.com>
2022-11-02 13:29:56 -0700

    Initial commit
    Signed-off-by: Rob Ranf <rob@emiyaconsulting.com>

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script>
+	import TheNavBar from "./components/TheNavBar.vue";
+	import TheFooter from "./components/TheFooter.vue";
+
+	export default {
+		components: {
+			TheNavBar,
+			TheFooter,
+		},
+	};
+</script>
+
+<template>
+	<TheNavBar />
+	<RouterView />
+	<TheFooter />
+</template>
+
+<style scoped></style>
---
+++ src/constants.ts
@@ -0,0 +1,7 @@
+export const restaurantStatusList = [
+	"Want to Try",
+	"Recommend",
+	"Do Not Recommend",
+];
+
+export const dietList = ["Vegetarian", "Pescetarian", "Keto"];
